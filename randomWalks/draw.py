925071ababf6af6541712f6a6a6aa87f94d8255e
libido <leonardovlibido@gmail.com>
2016-11-07 01:23:52 +0100

    improved line drawing functions

--- randomWalks/draw.py
+++ randomWalks/draw.py
@@ -17,17 +17,123 @@ def drawSegment( matrix, shapeLength):
         shapeLength -= 1
 
 def drawLine( matrix, lineLength):
-    i,j = 15,15
-    segLength = 8
+    random.seed()
+    i,j = len(matrix)-1,15
+    segLength = 5
     segNumber = lineLength/segLength
     while segNumber>0:
         direction = random.randint(0,4)
-        i,j = drawLineSegment(matrix, i, j, direction, segLength) 
+        i,j = drawLineSegment(matrix, i, j, "up", segLength) 
         segNumber -= 1
 
-#TODO fix index out of bounds.
+
+#TODO copy-pasted from up, needs sorting out
+def drawLineSegmentLeft( matrix, i, j, segLength):
+    height = len(matrix)
+    width = len(matrix[0])
+    while segLength>0:
+        x = random.randint(-1,1)
+        y = random.randint(-1,1)
+        if i == height:
+            break
+        if j+y<0 or j+y>width:
+            y *= -1
+        i += 1
+        j += y
+        if y == -1:
+            matrix[i][j] = '/'
+        elif y == 1:
+            matrix[i][j] = '\\'
+        else:
+            matrix[i][j] = '|'
+        segLength -= 1
+    return i,j
+
+#TODO copy-pasted from up, needs sorting out
+def drawLineSegmentRight( matrix, i, j, segLength):
+    height = len(matrix)
+    width = len(matrix[0])
+    while segLength>0:
+        x = random.randint(-1,1)
+        y = random.randint(-1,1)
+        if i == height:
+            break
+        if j+y<0 or j+y>width:
+            y *= -1
+        i += 1
+        j += y
+        if y == -1:
+            matrix[i][j] = '/'
+        elif y == 1:
+            matrix[i][j] = '\\'
+        else:
+            matrix[i][j] = '|'
+        segLength -= 1
+    return i,j
+
+#TODO this is the best function, all the others should look like this one.
+def drawLineSegmentUp( matrix, i, j, segLength):
+    height = len(matrix)
+    width = len(matrix[0])
+    while segLength>0:
+        x = random.randint(-1,1)
+        y = random.randint(-1,1)
+        if i == 0:
+            break
+        if j+y<0 or j+y>width:
+            y *= -1
+        if y == -1:
+            matrix[i][j] = '\\\\'
+        elif y == 1:
+            matrix[i][j] = '//'
+        else:
+            matrix[i][j] = '||'
+        segLength -= 1
+        i -= 1
+        j += y
+    return i,j
+
+#TODO changed i,j moving should be tested
+def drawLineSegmentDown( matrix, i, j, segLength):
+    height = len(matrix)
+    width = len(matrix[0])
+    while segLength>0:
+        x = random.randint(-1,1)
+        y = random.randint(-1,1)
+        if i == height:
+            break
+        if j+y<0 or j+y>width:
+            y *= -1
+        if y == -1:
+            matrix[i][j] = '/'
+        elif y == 1:
+            matrix[i][j] = '\\'
+        else:
+            matrix[i][j] = '|'
+        segLength -= 1
+        i += 1
+        j += y
+    return i,j
+
+#TODO add right and left calls after they are implemented, enumerate direction
 def drawLineSegment( matrix, i, j, direction, segLength):
     #direction = random.randint(0,4)
+    if direction == "up":
+        i,j = drawLineSegmentUp(matrix, i, j, segLength)
+    elif direction == "down":
+        i,j = drawLineSegmentDown(matrix, i, j, segLength)
+
+    elif direction == "right":
+        x=1
+        #drawLineSegmentRight()
+    elif direction == "left":
+        x=1
+        #drawLineSegmentLeft()
+    return i,j
+
+
+#TODO add checks for index overfow,
+def drawLineSegmentv0( matrix, i, j, direction, segLength):
     while segLength>0:
         x = random.randint(-1, 1)
         if direction == 0:
@@ -52,6 +158,6 @@ def printMatrix(matrix):
 
 
 matrix = createMatrix(30, 30)
-drawLine(matrix, 50)
+drawLine(matrix, 25)
 printMatrix(matrix)
 

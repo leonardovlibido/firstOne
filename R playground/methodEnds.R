5636d7730651fa5d2d32565489d55796c656a9b2
libido <leonardovlibido@gmail.com>
2016-11-10 02:31:00 +0100

    Vectorized the function

--- R playground/methodEnds.R
+++ R playground/methodEnds.R
@@ -15,3 +15,34 @@ methodEnds = function (sampleSize){
     }
     return(numTimesIsBigger/sampleSize)
 }
+
+methodEnds2 = function (sampleSize){
+    numTimesBigger = 0
+    triangleSideSquared = 3
+    alpha = runif(sampleSize,0,360)
+    beta = runif(sampleSize,0,360)
+    x1 = cos(alpha)
+    x2 = sin(alpha)
+    y1 = cos(beta)
+    y2 = sin(beta)
+    chrodSquared = (x1-y1)^2 + (x2-y2)^2
+    numTimesBigger = chrodSquared - triangleSideSquared
+    numTimesBigger = sign(numTimesBigger)
+    numTimesBigger = numTimesBigger + 1
+    numFinal = sum(numTimesBigger)/2
+    return(numFinal/sampleSize)
+}
+
+
+
+methodEnds3 = function (sampleSize){
+    numTimesBigger = 0
+    triangleSideSquared = 3
+    alpha = runif(sampleSize,0,360)
+    beta = runif(sampleSize,0,360)
+    numTimesBigger = sign((cos(alpha)-cos(beta))^2 + (sin(alpha)-sin(beta))^2 - triangleSideSquared)
+    numFinal = sum(numTimesBigger[numTimesBigger>0])
+    return(numFinal/sampleSize)
+}
+
+
